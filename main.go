0b4bc7941ffd462134579d4e6dea8e9f6dbea269
Stan Choi <sjc591@nyu.edu>
2024-07-08 00:07:14 -0400

    Build simplest shell

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Coding Challenges Shell")
+	// 1 display the prompt
+	fmt.Print("ccsh> ")
+
+	// 2 read a line of input from the user
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+
+	// 3 & 4 prepare the Command to be run, capturing the input
+	cmd := exec.Command(input)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Run()
 }
