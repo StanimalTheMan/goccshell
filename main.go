678f06055ff5965d7612a86c753c82c0c0cff340
Stan Choi <sjc591@nyu.edu>
2024-07-25 21:13:57 -0400

    Attempt to handle multiple inputs and exit shell

--- main.go
+++ main.go
@@ -9,18 +9,22 @@ import (
 )
 
 func main() {
-	// 1 display the prompt
-	fmt.Print("ccsh> ")
+	for {
+		// 1 display the prompt
+		fmt.Print("ccsh> ")
 
-	// 2 read a line of input from the user
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	input = strings.TrimSpace(input)
+		// 2 read a line of input from the user
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		if input == "exit" {
+			break
+		}
+		// 3 & 4 prepare the Command to be run, capturing the input
+		cmd := exec.Command(input)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
 
-	// 3 & 4 prepare the Command to be run, capturing the input
-	cmd := exec.Command(input)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	cmd.Run()
+		cmd.Run()
+	}
 }
